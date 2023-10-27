54d033f575cddd5a58a5d2534ec95dc49a7118a6
malcomowilla <owillamalcom@gmail.com>
2023-10-27 19:38:59 +0300

    i have made changes to make the website responsive after integraing with  tailwind for a smooth user experience

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Get all the navigation links
     const navLinks = document.querySelectorAll('.nav-links ul li a');
+    const button = document.querySelector(".font-btn" )
 
+    button.addEventListener('click',()=> {
+        const aboutMe = document.querySelector('.container7');
+        aboutMe.scrollIntoView({behavior: 'smooth'})
+    })
     // Smooth scrolling for navigation links
     navLinks.forEach(link => {
         link.addEventListener('click', function(e) {
@@ -30,6 +35,12 @@ document.addEventListener("DOMContentLoaded", function() {
             }else if(e.target.innerText === 'About Me'){
                 const projects = document.querySelector('.container2')
                 projects.scrollIntoView({ behavior: 'smooth' });
+
+
+
+            }else if(e.target.innerText === 'My Socials'){
+                    const socials = document.querySelector('.container1')
+                    socials.scrollIntoView({behavior: 'smooth'})
 
             }
             
@@ -72,33 +83,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // observer.observe(container1)
 
-const container3 = document.querySelector('.container3')
+// const container3 = document.querySelector('.container3')
 
 
 
-const observer2 = new IntersectionObserver((entries)=>{
+// const observer2 = new IntersectionObserver((entries)=>{
 
-entries.map((entry)=>{
+// entries.map((entry)=>{
 
-    entry.target.classList.toggle('container3-animation', entry.isIntersecting)
-    if (entry.isIntersecting) {
-        observer2.unobserve(entry.target)
-    }
-    // if (entry.isIntersecting) {
-    //     entry.target.classList.add('container3-animation')
-        
-    // } else {
-    //     entry.target.classList.remove('container3-animation')
-    // }
-})
+//     entry.target.classList.toggle('container3-animation', entry.isIntersecting)
+//     if (entry.isIntersecting) {
+//         observer2.unobserve(entry.target)
+//     }
+    
+// })
 
-}, {
-    threshold: 0.5
-})
+// }, {
+//     threshold: 0.5
+// })
 
 
 
-observer2.observe(container3)
+// observer2.observe(container3)
 
 const sr = ScrollReveal({
     duration: 3000,
@@ -109,15 +115,14 @@ const sr = ScrollReveal({
 
 
 const sr2 = ScrollReveal({
-    duration: 5000,
+    duration: 7000,
     reset: true,
 })
-sr.reveal('.container5', {origin: 'left'})
+sr.reveal('.container5', {origin: 'top'})
 
 
 
 sr2.reveal('.container4')
-
 
 
 
@@ -127,7 +132,28 @@ const sr3 = ScrollReveal({
 })
 
 sr3.reveal('.header', {origin: 'top'})
+// sr3.reveal('.container2', {origin: 'top'})
 
+
+const sr5 = ScrollReveal({
+    duration: 1000,
+    reset: true,
+})
+
+ sr5.reveal('.container2', {origin: 'left'})
+
+
+
+
+
+ const sr6 = ScrollReveal({
+    duration:1600,
+    reset: true,
+    distance: '40px'
+
+ })
+
+ sr6.reveal('.container3',{origin: 'top'})
 
 
 const sr4 = ScrollReveal({
@@ -161,15 +187,13 @@ try {
 
 
 
+const fontIcon = document.querySelector('.fa-solid.fa-bars');
+const header = document.querySelector('header');
 
-
-
-
-
-
-
-
-
+fontIcon.addEventListener('click', (e) => {
+    console.log(e)
+    header.classList.toggle('header1');
+});
 
 
 
